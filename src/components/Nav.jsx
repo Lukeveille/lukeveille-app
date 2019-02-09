f95ccf79065d05f8dac694d4b02917dca5c21470
Lukeveille <luke@lukeleveille.com>
2019-02-09 17:17:02 -0500

    set up main template and nav bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Nav = () => {
+  return (
+    <header>
+      <h1 className="main-header">Luke Leveille</h1>
+      <h2>Full Stack Web Developer</h2>
+      <nav>
+        <a href="#skills" className="first">Skills</a><a href="#about">About Me</a><a href="#projects">Projects</a><a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
